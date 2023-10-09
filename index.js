0b2e2dd58a20fffa236e50f0a09d9c932095c50a
shakhina04 <sheryl022604@gmail.com>
2023-10-09 15:14:17 +0500

    goods_cart_1.2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let container = document.querySelector(".container");
 let showAll = document.querySelector("#showAll");
 let showFive = document.querySelector("#showFive");
 let h1 = document.querySelector("h1");
-let cart_container = document.querySelector('.scrollable')
+let cart_container = document.querySelector(".scrollable");
 let cart = [];
 let menu = document.getElementById("menu");
 
@@ -15,10 +15,8 @@ showFive.onclick = () => {
 
 reload(arr);
 
-
 function reload(massiv) {
-  container.innerHTML = ""
-
+  container.innerHTML = "";
 
   for (let elem of massiv) {
     let item = document.createElement("div");
@@ -81,7 +79,6 @@ function reload(massiv) {
     rateWrap.append(rateImage, rate);
     countWrap.append(countImage, count);
 
-
     // d
     buttonAdd.onclick = () => {
       if (cart.includes(elem.id)) {
@@ -96,105 +93,156 @@ function reload(massiv) {
         buttonAdd.innerHTML = "Добавлено";
       }
 
-      cart_reload(cart)
+      cart_reload(cart);
       h1.innerHTML = `Cart: ${cart.length}`;
     };
-
-
 
     // ///////////////////////////////
     // buttonAdd.onclick = () => {
     //   modalka.append(item);
     // };
-
   }
 }
 
-cart_reload(cart)
+cart_reload(cart);
 
 function cart_reload(cart_ids) {
   cart_container.innerHTML = "";
-  let temp = []
+  let temp = [];
 
-  for(let product of arr) {
-    for(let id of cart_ids) {
+  for (let product of arr) {
+    for (let id of cart_ids) {
       if (id === product.id) {
-        temp.push(product)
+        temp.push(product);
+      }
+    }
+  }
+
+  for (let item of temp) {
+    let cart_item = document.createElement("div");
+    let div_left = document.createElement("div");
+    let left_inp = document.createElement("input");
+    let left_img = document.createElement("img");
+
+    let div_mid = document.createElement("div");
+    let mid_title = document.createElement("span");
+    let mid_details = document.createElement("div");
+    let details_div = document.createElement("div");
+    let details_p_one = document.createElement("p");
+    let details_p_two = document.createElement("p");
+    let counter = document.createElement("div");
+    let counter_plus = document.createElement("button");
+    let counter_minus = document.createElement("button");
+    let counter_input = document.createElement("input");
+    let under_counter = document.createElement("div")
+    let counter_and_under_counter = document.createElement("div")
+
+    let div_right = document.createElement("div");
+    let right_button = document.createElement("button");
+    let right_button_img = document.createElement("img");
+    let right_price = document.createElement("span");
+    let right_sale_price = document.createElement("span");
+
+    cart_item.classList.add("cart-item");
+    div_left.classList.add("left");
+    div_mid.classList.add("mid");
+    div_right.classList.add("right");
+    mid_title.classList.add("title");
+    mid_details.classList.add("details");
+    counter.classList.add("counter");
+    right_price.classList.add("price");
+    right_sale_price.classList.add("sale-price");
+    under_counter.classList.add("under_counter")
+    counter_and_under_counter.classList.add("counter_and_under_counter")
+
+    left_inp.type = "checkbox";
+    left_img.src = item.image;
+    counter_input.type = "number";
+    counter_input.value = 1;
+    right_button_img.src = "./icons/trash.svg";
+
+    mid_title.innerHTML =
+      item.title.length > 20 ? item.title.slice(0, 17) + "..." : item.title;
+    details_p_one.innerHTML = `Категория: ${item.category}`;
+    details_p_two.innerHTML = `В наличии: ${item.rating.count}`;
+    counter_plus.innerHTML = "+";
+    counter_minus.innerHTML = "-";
+    under_counter.innerHTML = `${item.price} сум/ед.`
+    right_button.innerHTML = "delete";
+    right_price.innerHTML = `${item.price} usd`;
+    let salePrice = item.price - (10 * item.price) / 100;
+    right_sale_price.innerHTML = `${salePrice}`;
+
+    cart_item.append(div_left, div_mid, div_right);
+    div_left.append(left_inp, left_img);
+    div_mid.append(mid_title, mid_details);
+    mid_details.append(details_div,counter_and_under_counter);
+    counter_and_under_counter.append(counter, under_counter)
+    details_div.append(details_p_one, details_p_two);
+    counter.append(counter_plus, counter_input, counter_minus);
+    div_right.append(right_button, right_price, right_sale_price);
+    right_button.prepend(right_button_img);
+    cart_container.append(cart_item);
+
+    right_button.onclick = () => {
+      let idx = cart.indexOf(item.id);
+      cart.splice(idx, 1);
+      cart_item.remove();
+    };
+    ////////////////////
+    // right_button.onclick = () => {
+    //   // cart_reload()
+
+    //   if (cart.includes(elem.id)) {
+
+    //     buttonAdd.classList.remove("active");
+    //     buttonAdd.innerHTML = "В избранное";
+    //   }
+
+    // }
+    ////////////////////
+
+    // right_price.innerHTML = `${item.price} usd`;
+    // let salePrice = item.price - (10 * item.price) / 100;
+    // right_sale_price.innerHTML = `${salePrice}`;
+
+    let number = 1;
+
+    counter_plus.onclick = function() {
+      if (number <= 4) {
+        number++
+        counter_input.value = number;
+        under_counter.style.display = "flex"
+
+        item.price = item.price + item.price
+        right_price.innerHTML = item.price 
+
+        salePrice = salePrice + salePrice
+        right_sale_price.innerHTML = salePrice
+
       }
     }
 
-  }
+    counter_minus.onclick = function() {
+      if (number >= 2) {
+          number--;
+          counter_input.value = number;
+          under_counter.style.display = "none"
 
-  for(let item of temp) {
-    let cart_item = document.createElement('div')
-    let div_left = document.createElement('div')
-    let left_inp = document.createElement('input')
-    let left_img = document.createElement('img')
-    
-    let div_mid = document.createElement('div')
-    let mid_title = document.createElement('span')
-    let mid_details = document.createElement('div')
-    let details_div = document.createElement('div')
-    let details_p_one = document.createElement('p')
-    let details_p_two = document.createElement('p')
-    let counter = document.createElement('div')
-    let counter_plus = document.createElement('button')
-    let counter_minus = document.createElement('button')
-    let counter_input = document.createElement('input')
+          item.price = item.price - item.price
+          right_price.innerHTML = item.price
 
-    let div_right = document.createElement('div')
-    let right_button = document.createElement('button')
-    let right_button_img = document.createElement('img')
-    let right_price = document.createElement('span')
-    let right_sale_price = document.createElement('span')
+          
 
-    cart_item.classList.add('cart-item')
-    div_left.classList.add('left')
-    div_mid.classList.add('mid')
-    div_right.classList.add('right')
-    mid_title.classList.add('title')
-    mid_details.classList.add('details')
-    counter.classList.add('counter')
-    right_price.classList.add('price')
-    right_sale_price.classList.add('sale-price')
+       }
+   }
 
-    left_inp.type = "checkbox"
-    left_img.src = item.image
-    counter_input.type = "number"
-    counter_input.value = 1
-    right_button_img.src = "./icons/trash.svg"
 
-    mid_title.innerHTML = item.title.length > 20 ? item.title.slice(0,17) + "..." : item.title
-    details_p_one.innerHTML = `Категория: ${item.category}`
-    details_p_two.innerHTML = `В наличии: ${item.rating.count}`
-    counter_plus.innerHTML = '+'
-    counter_minus.innerHTML = '-'
-    right_button.innerHTML = 'delete'
-    right_price.innerHTML = `${item.price} usd`
-    let salePrice = item.price - (10 * item.price / 100)
-    right_sale_price.innerHTML = `${salePrice}`
 
-    cart_item.append(div_left, div_mid, div_right)
-    div_left.append(left_inp, left_img)
-    div_mid.append(mid_title, mid_details)
-    mid_details.append(details_div, counter)
-    details_div.append(details_p_one, details_p_two)
-    counter.append(counter_plus, counter_input, counter_minus)
-    div_right.append(right_button, right_price, right_sale_price)
-    right_button.prepend(right_button_img)
-    cart_container.append(cart_item)
 
-    right_button.onclick = () => {
-      let idx = cart.indexOf(item.id)
-      cart.splice(idx, 1)
-      cart_item.remove()
-  } 
- 
+
   }
 }
-
-
-
 
 // MODAL WINDOW
 let modalka = document.querySelector(".modalka");
